272e19a1548e9171589eafa6aaa2e65f8a32dff2
torrento <sachinles99@gmail.com>
2023-08-30 18:57:01 +0530

    added

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-const courses = [
-    {id: 2, name: "a"},
-    {id: 1,name: "b"},
-  ];
-  
-  const cour =  courses.find(element => element.id == '2')
-  const courOne =  courses.find(element => element.name == 'a')
-  console.log(cour)
-  console.log(courOne)
+let a = "new"
+
+if (!a < 2 ){
+  console.log(a);
+}
