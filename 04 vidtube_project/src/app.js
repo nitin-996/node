414bb53987c891ffb3db40a6326cfaa02fde97f6
Nitin <sachinles99@gmail.com>
2025-04-01 18:49:35 +0530

    implemented jwt and bcrypt in user model

--- 04 vidtube_project/src/app.js
+++ 04 vidtube_project/src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-
+import cookieParser from "cookie-parser"
 const app = express()
 var corsOptions = {
     origin: process.env.CORS_ORIGIN,
@@ -14,7 +14,7 @@ var corsOptions = {
   app.use(express.json({limit:"16kb"}))
   app.use(express.static("public"))
   app.use(express.urlencoded({extended:true,limit:"16kb"}))
-
+  app.use(cookieParser())
 
 // Routes
 import healthcheckRouter from "./routes/healthcheck.routes.js"
--- 04 vidtube_project/src/models/user.models.js
+++ 04 vidtube_project/src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const { Schema } = mongoose;
 
@@ -49,6 +51,62 @@ const userSchema = new Schema({
 },
 {timestamps:true}
 )
+
+
+// using mongoDB middleware which will help to encrypt password securely
+// also here we are using normal function bcz we need to reference of userschema
+// on which we are applying pre middleware from mongo. it is not from express.
+userSchema.pre("save", async function(next){
+
+    if(!this.isModified("password")){
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password,10);
+})
+
+
+
+// using schema methods function , here we are defining own 
+// custom methods.
+userSchema.methods.ispasswordCorrrect = async function(password){
+
+    // this compare functions returns true or false
+    const result = bcrypt.compare(password,this.password)
+    return result
+}
+
+
+// access token
+userSchema.methods.generateAccessToken =  async function(){
+
+    var token = jwt.sign(
+        {
+            _id : this._id,
+            username: this.username,
+            email: this.email
+             },
+         process.env.Access_Token_Secret,
+        {expiresIn: "1h"});
+
+    return token    
+
+}
+
+// refresh token
+userSchema.methods.generateRefreshToken =  async function(){
+
+    var token = jwt.sign(
+        {
+            _id : this._id,
+             },
+         process.env.Refresh_Token_Secret,
+        {expiresIn: "10d"});
+
+    return token    
+
+}
+
 
 const User = mongoose.model("User" , userSchema)
 
